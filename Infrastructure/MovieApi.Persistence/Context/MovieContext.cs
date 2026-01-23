7334855c620e5dd490f8659be4a441f1846a78f1
yaren0600 <begumozturk0600@gmail.com>
2026-01-24 01:37:33 +0300

    24.01.26

--- Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,9 @@ using System.Threading.Tasks;
 
 namespace MovieApi.Persistence.Context
 {
-    internal class MovieContext
+    public class MovieContext : DbContext
     {
 
+        
     }
 }
