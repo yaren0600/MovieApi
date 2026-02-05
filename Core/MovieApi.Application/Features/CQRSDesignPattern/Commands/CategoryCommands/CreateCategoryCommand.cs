a8768d1de552cb9ea706fa685e0c729108590dd7
yaren0600 <begumozturk0600@gmail.com>
2026-02-06 01:32:13 +0300

    CQRS (6.2.26)

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Commands/CategoryCommands/CreateCategoryCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Commands.CategoryCommands
+{
+    public class CreateCategoryCommand
+    {
+        public int CategoryName { get; set; }
+    }
+}
