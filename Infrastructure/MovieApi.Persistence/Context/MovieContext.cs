a34a53e0c4bcd3bb933808ec8a3ee4ceac754458
yaren0600 <begumozturk0600@gmail.com>
2026-02-05 23:46:55 +0300

    5.2.26

--- Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +9,22 @@ using System.Threading.Tasks;
 namespace MovieApi.Persistence.Context
 {
     public class MovieContext : DbContext
-      /*Bu kullanım, .NET dünyasında Entity Framework Core (EF Core) kütüphanesinin temel taşını oluşturur. 
-       * DbContext sınıfından miras alan MovieContext sınıfı, uygulamanız ile veritabanı arasındaki köprüdür.*/
+    /*Bu kullanım, .NET dünyasında Entity Framework Core (EF Core) kütüphanesinin temel taşını oluşturur. 
+     * DbContext sınıfından miras alan MovieContext sınıfı, uygulamanız ile veritabanı arasındaki köprüdür.*/
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Veritabanı bağlantı dizesi burada belirtilir.
+            optionsBuilder.UseSqlServer("Server=DESKTOP-MBOHETQ\\SQLEXPRESS;initial Catalog=ApiMovieDb;" +
+                "integrated Security=true;" +
+                "TrustServerCertificate=true");
 
-        
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Cast> Casts { get; set; }
     }
 }
