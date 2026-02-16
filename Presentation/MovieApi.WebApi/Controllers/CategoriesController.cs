f75c4e5ab54369510b124d9982fd94594abab28b
yaren0600 <begumozturk0600@gmail.com>
2026-02-17 00:01:00 +0300

    2.17.26

---
+++ Presentation/MovieApi.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.CQRSDesignPattern.Queries.CategoryQueries;
+
+namespace MovieApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        //private readonly 
+    }
+}
